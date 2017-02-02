6158d144c8e7299559c7e08efb66f127e7c0cae7
hi2suresh <hi2suresh@gmail.com>
2017-02-02 10:06:22 +1000

    Fixed CSS and made code changes to align images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-//Script uses jquery and hence to be run after loading jQuery
+/*=============================================================
+jQuery Script to search and list images based on image caption
+==============================================================*/
 
-$('#search-input').on('input', function(e) {
+$('#search-input').on('input', function (e) {
     var inputValue = $(this).val().toLocaleLowerCase();
     var imageTitle = '';
-   $('.gallery li').filter(function(){
-       //Get the image title or caption
-       imageTitle = $(this).children('a').attr('title').toLowerCase();
-       //Show the images only if the caption includes input value.
-       if(imageTitle.includes(inputValue)){         
-          $(this).css('display','');           
-          }
-       else{
-            $(this).css('display','none');
-       }
-   });    
+    var alignLeftCounter = 0;
+    $('.gallery li').filter(function () {
+        //Get the image title or caption
+        imageTitle = $(this).children('a').attr('title').toLowerCase();
+        //Show the images only if the caption includes input value.
+        if (imageTitle.includes(inputValue)) {
+            $(this).css('display', '');
+            alignLeftCounter++;
+        }
+        else {
+            $(this).css('display', 'none');
+        }
+    });
+    
+    //Align the images left or towards center
+    if (alignLeftCounter % 4 !== 0 && alignLeftCounter > 4) {
+        $('.gallery').css('text-align', 'left');
+    }
+    else {
+        $('.gallery').css('text-align', 'center');
+    }
+    
 });
